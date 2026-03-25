2ec8a24e4d7bfdbd0025b6bdeee37197b8f7e3d7
syllex <1654063802@qq.com>
2026-03-25 22:37:12 +0100

    New files

--- src/utils/server/util_server.cpp
+++
@@ -1,7 +0,0 @@
-#include "util_server.h"
-#define PRINT_ERROR std::cout<<"Unexpected Error Occurred : 14";
-
-std::string handle_server_request(int socket){
-
-}
-
--- src/utils/server/util_server.h
+++
@@ -1,5 +0,0 @@
-#ifndef UTILS_4
-#define UTILS_4
-#include <string>
-std::string handle_server_request(int socket);
-#endif 
