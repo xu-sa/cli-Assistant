8cfe8a9e2d403c0f7d9163da6262ad7993e5a8ad
syllex <1654063802@qq.com>
2026-03-28 21:47:03 +0100

    test app

---
+++ test/main.cpp
@@ -0,0 +1,29 @@
+#include "./lib/syagent_interface.h"
+#include <iostream>
+using namespace std;
+//~/.local/share
+#ifdef _WIN32
+#define DEFAULT_HOME argv[0]
+#define DEFAULT_ROOM "windows-agent"
+#define DEFAULT_PORT 9995
+#else 
+#define DEFAULT_HOME argv[0]
+#define DEFAULT_ROOM "linux-agent"
+#define DEFAULT_PORT 9995
+#endif
+int main(int argc,const char** argv) {
+    sagtlib::sagent* agent = sagtlib::sagent::create((argc<2?DEFAULT_HOME:argv[1]),(argc<3?DEFAULT_ROOM:argv[2]));
+    if (agent!=NULL) {
+        if(argc<4){
+            agent->terminalsession(1);
+        }else{
+            agent->start_server_thread(argv[3]);    
+            agent->terminalsession(0);
+        }
+        sagtlib::sagent::destroy(agent);
+    } else {
+        std::cerr << "failed to create agent!" << std::endl;
+        return 1;
+    }    
+    return 0;
+} 
